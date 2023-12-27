2d67ae3b91386247e523aa5e7476b2d84fbd33e2
Harminder Virk <virk.officials@gmail.com>
2023-12-27 13:37:09 +0530

    chore: update dependencies

--- bin/test.ts
+++ bin/test.ts
@@ -1,40 +1,10 @@
 import { assert } from '@japa/assert'
-import { pathToFileURL } from 'node:url'
-import { specReporter } from '@japa/spec-reporter'
-import { runFailedTests } from '@japa/run-failed-tests'
-import { processCliArgs, configure, run } from '@japa/runner'
+import { processCLIArgs, configure, run } from '@japa/runner'
 
-/*
-|--------------------------------------------------------------------------
-| Configure tests
-|--------------------------------------------------------------------------
-|
-| The configure method accepts the configuration to configure the Japa
-| tests runner.
-|
-| The first method call "processCliArgs" process the command line arguments
-| and turns them into a config object. Using this method is not mandatory.
-|
-| Please consult japa.dev/runner-config for the config docs.
-*/
+processCLIArgs(process.argv.slice(2))
 configure({
-  ...processCliArgs(process.argv.slice(2)),
-  ...{
-    files: ['tests/**/*.spec.ts'],
-    plugins: [assert(), runFailedTests()],
-    reporters: [specReporter()],
-    importer: (filePath) => {
-      return import(pathToFileURL(filePath).href)
-    },
-  },
+  files: ['tests/**/*.spec.ts'],
+  plugins: [assert()],
 })
 
-/*
-|--------------------------------------------------------------------------
-| Run tests
-|--------------------------------------------------------------------------
-|
-| The following "run" method is required to execute all the tests.
-|
-*/
 run()
